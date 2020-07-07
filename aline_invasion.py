61f395f98cf4546db03e8403b90ec5368f8b26eb
Jonathan Garache <jongarache@gmail.com>
2020-07-06 22:28:24 -0700

    Ships movement and limitations + Keypresses
    Movement
    I setup ship movement as a boolean so that when 'a' or 'd' are pressed, the boolean is set to true and the ships movement will go continuously by a factor of 1.5 pixels. When the key is released, the boolean is set to false and the movement of the ship has stopped.
    
    Limitations
    I set it so that that the ship is limited to the boundaries of the window.
    
    Key presses
    a - left 
    d - right
    q - quit game

--- aline_invasion.py
+++ aline_invasion.py
@@ -15,7 +15,14 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        """Windows mode"""
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        """Full screen mode
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        """
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -32,15 +39,41 @@ class AlienInvasion:
             # redraw the screen during each pass through the loop
             self._update_screen()
 
-    @staticmethod
-    def _check_events():
+    def _check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Responds to keypress"""
+        if event.key == pygame.K_d:
+            # move the ship to the right
+            self.ship.moving_right = True
+
+        elif event.key == pygame.K_a:
+            # move the ship to the left
+            self.ship.moving_left = True
+
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Responds to key releases"""
+        if event.key == pygame.K_d:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_a:
+            self.ship.moving_left = False
+
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        self.ship.update()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
--- ship.py
+++ ship.py
@@ -8,13 +8,31 @@ class Ship:
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
-        # load the ship image and get its rect.
+        # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
-        # start each new ship at the bottom center of the screen.
+        # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ships horizontal positions
+        self.x = float(self.rect.x)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ships movement position based on the movement flag"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """draw the ship at its current location"""
